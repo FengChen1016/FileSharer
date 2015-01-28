bc926e31cce65d654b0a971dea51586bf147972d
Chester <fchenf@cn.ibm.com>
2015-01-28 11:12:08 +0800

    update script

--- nc.sh
+++ nc.sh
@@ -1,26 +1,29 @@
 #!/bin/bash
-currentDate=`date`
-t0=`date -d $currentDate +%s`  # convert to long timestamp
 
+### information ###
+currentDate=`date`
+t0=`date -d "$currentDate" +%s`  # convert to long timestamp
 echo "$0 $$ Start network checking..."
 
-#address=192.168.0.119  // not exist for debug
+### variable definition and preparation ###
+#address=192.168.0.119  # not exist for debug
 address=www.baiduc.com
 reportFile=report.txt
-maxSize=(( 1024 * 1024))  # 1 Mb
+touch $reportFile  # make sure this file exist
+maxSize=`expr 1024 \* 1024`  # 1 Mb
 duration=3600  # 1 hour
 total=1
 num=1
+
+### execute ###
 while true
 do
 	result=`ping -c 1 ${address}`
 	total=`expr ${total} + 1`
 	returnCode=$?
 	# if [[ $string == *"some string"* ]]
-	if [[ ${returnCode} -eq 0 ]] 
+	if [[ ${returnCode} -ne 0 ]] 
 	then
-		# ping success, do nothing
-	else
 		num=`expr ${num} + 1`
 		echo "${num}/${total} : ${result}" >> $reportFile
 	fi
@@ -31,9 +34,9 @@ do
 	# use [[ ]] to compare strings, use (( )) to compare numbers
 	#Get file size in bytes
 	reportSize=`stat -c%s $reportFile`
-	if (( `expr $t1 + $duration` > $t0 )) || (( $reportSize > $maxSize ))
+	# break if runing for specified time or log file size > maxSize
+	if (( `expr $t0 + $duration` > $t1 )) || (( $reportSize > $maxSize ))
     then
         break
     fi
 done
-return 0
